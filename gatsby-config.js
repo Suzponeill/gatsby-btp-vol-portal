3e08bd74f3ffad4ae9501f33158d33e7a5791bec
SUZANNE ONEILL <SUZPONEILL@GMAIL.COM>
2023-01-31 12:46:49 -0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `gatsby_btp_vol_portal`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
